d3459a329846db1e6f00c5d2d50a98de17f7c479
Jared Gallina <jdg272@cornell.edu>
2019-12-03 18:28:04 -0500

    TODOs in op.py for read_toastiness

--- op.py
+++ op.py
@@ -3,7 +3,7 @@
 #this script is to run the sub programs of SMORE project
 import user_input as usr
 import pick_contour as pc
-import RPi.GPIO as GPIO
+import read_toastiness as rt
 
 if __name__ == "__main__":
     #getting the desired contour for the mallow
@@ -16,4 +16,13 @@ if __name__ == "__main__":
     print("Returned Value is:")
     print(TL)
 
+    #reading the toastiness of the contour until desired level is reached
+    #TODO
+    #make read toastwork for a main function with args
+    #make read toast be responsible for rotating servo with mallow
+    #make RT handle turning on and off heating element
+    rt.main(TL,C)   
 
+
+    #TODO
+    #need to implement a script that will grab the mallow
