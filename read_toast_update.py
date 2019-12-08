077956e4091d426f1d3e539f2d4755870e538bff
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-12-08 00:38:05 -0500

    Create read_toast_update.py

---
+++ read_toast_update.py
@@ -0,0 +1,112 @@
+#!/usr/bin/python
+
+#this is a script to read in camera data of a detected marshmallow, and read it's toastedness
+#toastedness will be done via averaging pixel data of detected marshmallow shape
+#script also rotates the toasting stepper and 
+#TODO
+#controls heating element activation
+
+import sys
+import cv2
+import numpy as np
+import imutils
+
+#basic color definitions
+RED = (255,0,0)
+BLACK = (0,0,0)
+WHITE = (255,255,255)
+
+#contour that is the mallow
+Mallow_Cont = []
+#desired toastedness of the mallow
+Toast_Level = 0
+
+
+#this function takes in a grayscale image and a given contour to find
+#the darkening of the specific region bound by the contour
+def RoastLevel(img):
+
+    global Mallow_Cont
+    c = Mallow_Cont
+    
+    #finding the pixels in a given contour and creating a mask of those pixels
+    mask = np.zeros_like(img)
+    cv2.drawContours(mask, c,-1, color=255,thickness =-1)
+
+    #recording the pixels on the interior of the mask
+    pixs = np.where(mask==255)
+
+    #initialize several useful variables
+    pix_dark = 0    #total darkness value of the region
+    count = 0       #number of pixels counted
+    roast_level = 0 #overall roast level
+
+    #ensuring that there are some interior pixels
+    if(len(pixs[0]) > 0):
+
+        #working through all interior pixels
+        for i in range(len(pixs[0])):
+        
+            #summing up the grayscale value for level of "darkening
+            pix_dark += img[pixs[0][i],pixs[1][i]]
+            count +=1
+
+        #final calculation of darkness
+        roast_level = pix_dark/float(count)
+    
+    return roast_level
+
+
+def main(TL,C):
+
+    #global
+    global Mallow_Cont
+    global Toast_level
+    Mallow_Cont = C
+    Toast_Level = TL
+    
+    #basic video capture object
+    videoCap = cv2.VideoCapture(0)
+
+    #checking successful video capture creation
+    if(not videoCap.isOpened()):
+        print ("ERROR CANT FIND PI CAMERA")
+        quit()
+    else:
+        print("camera found")
+
+
+    #reading in an initial frame
+    b, image = videoCap.read()
+
+    #continuously reading in camera data
+    while True:
+
+        #reading in a frame
+        b, image = videoCap.read()
+
+        #adjusting for more processing
+        im_resize = imutils.resize(image, width=300)
+        ratio = image.shape[0]/float(im_resize.shape[0])
+    
+        #image processing steps
+        gray = cv2.cvtColor(im_resize, cv2.COLOR_BGR2GRAY)  #grayscale
+
+        
+        #displaying the toastedness level of the desired contour
+        #location of printing
+        c_x = 300  
+        c_y = 75
+        
+        #draw contour 
+        cv2.drawContours(image, [Mallow_Cont], -1, RED,-1)
+        cv2.putText(image, status, (c_x,c_y), cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 2)
+        
+        #displaying the associated toastedness
+        roastLevel = RoastLevel(c,gray)
+        cv2.putText(image, str(roastLevel), (300,100), cv2.FONT_HERSHEY_SIMPLEX, 0.5, WHITE, 2)
+
+
+        cv2.imshow("Gray", gray)
+        cv2.imshow("Image", image)
+        cv2.waitKey(1)
