368801891cffd540aee6732ed6fa29ef305b78a9
jared-gal <43042105+jared-gal@users.noreply.github.com>
2019-12-11 02:03:58 -0500

    Update retrieve.py

--- retrieve.py
+++ retrieve.py
@@ -14,24 +14,22 @@ def main():
     pin_claw = GPIO.PWM(6,46.382)
     pin_step = GPIO.PWM(4,100)
     
-    #for .5 seconds we open the claw
-    print ("OPEN CLAAW")
+    #for .2 seconds we open the claw
     pin_claw.start(7.236)
     start_time = time.time()
     while (time.time() - start_time) <.2:
         time.sleep(.01)
     pin_claw.start(0)
 
-    #for 2 seconds move the claw forward
-    print("FORWARD")
+    #for 5.5 seconds move the claw forward
     GPIO.output(19,1)
     pin_step.start(50)
     start_time = time.time()
-    while (time.time() - start_time) <5:
+    while (time.time() - start_time) <5.5:
         time.sleep(.01)
     pin_step.start(0)
 
-    #for .8 seconds close the claw
+    #for 1.2 seconds close the claw
     pin_claw.ChangeFrequency(46.642)
     pin_claw.start(6.716)
     start_time = time.time()
@@ -39,11 +37,11 @@ def main():
         time.sleep(.01)
     pin_claw.start(0)
 
-    #for 2 seconds we withdraw the claw
+    #for 7 seconds we withdraw the claw
     GPIO.output(19,0)
     pin_step.start(50)
     start_time = time.time()
-    while (time.time() - start_time) <6:
+    while (time.time() - start_time) <7:
         time.sleep(.01)
     pin_step.start(0)
 
